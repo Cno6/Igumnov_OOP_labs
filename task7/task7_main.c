d95642b3b33d9be2cfd1b1c61194195eedc7a171
Vadim Igumnov <vad.igumnoff@gmail.com>
2021-02-19 16:16:12 +0300

    Init commit

---
+++ task7/task7_main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "func.h"
+
+void main() {
+	const double x1 = 5.0;
+	x = x1;
+	f();
+	printf("f(5) = %.04lf\n", result);	
+	printf("Enter X = ");
+	scanf("%lf", &x);
+	f();
+	printf("f(%.0lf) = %.04lf\n", x, result);
+}
